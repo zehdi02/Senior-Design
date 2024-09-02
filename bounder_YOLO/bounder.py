b21253da55d2f84ca79a273cb1c160d1b2fdbef0
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2024-09-01 22:20:23 -0400

    created training loop

--- bounder_YOLO/bounder.py
+++ bounder_YOLO/bounder.py
@@ -1,3 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo8n.pt")
+def main():
+    model = YOLO("yolov8n.pt")
+
+    results = model.train(data='bounder.yaml', epochs=1, batch=8, device='cuda', optimizer='Adam', lr0=.5)
+
+
+if __name__ == '__main__':
+    main()
