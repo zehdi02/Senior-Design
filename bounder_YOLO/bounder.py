b6097579e4a1327d958c9170a0bc39bf7db2dccb
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2024-09-06 20:49:47 -0400

    created training loop for YOLOv8 on Manga109 dataset

--- bounder_YOLO/bounder.py
+++ bounder_YOLO/bounder.py
@@ -1,10 +1,40 @@
 from ultralytics import YOLO
+import matplotlib.pyplot as plt
+
 
 def main():
     model = YOLO("yolov8n.pt")
 
-    results = model.train(data='bounder.yaml', epochs=1, batch=8, device='cuda', optimizer='Adam', lr0=.5)
+    result_grid = model.train(
+        data='manga109.yaml',
+        epochs=10,
+        batch=16,
+        device='cuda',
+        dropout=.05,
+        val=True,
+        save=True,
+        resume=True,
+        cache=True
+    )
+
+    """
+    for i, result in enumerate(result_grid):
+        plt.plot(
+            result.metrics_dataframe["training_iteration"],
+            result.metrics_dataframe["mean_accuracy"],
+            label=f"Trial {i}",
+        )
+
+    plt.xlabel("Training Iterations")
+    plt.ylabel("Mean Accuracy")
+    plt.legend()
+    plt.show()
+    """
 
 
 if __name__ == '__main__':
     main()
+
+
+"""
+"""
