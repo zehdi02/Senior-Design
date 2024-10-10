514e0b50aed33cbf89e19d01e18d282683d6c2aa
Mahmud Hasan <28367258+QuodFinis@users.noreply.github.com>
2024-10-10 09:09:35 -0400

    function to agregate run results and create plot of all training

--- bounder_YOLO/bounder.py
+++ bounder_YOLO/bounder.py
@@ -1,39 +1,49 @@
+import os
 import cv2
 from ultralytics import YOLO
+from aggregate_runs import aggregate_run_results
+
+
+def test(model, image_path):
+    # Predict and return the result
+    results = model.predict(source=image_path, device='cuda')
+
+    # Load and display the original image with predictions
+    annotated_img = results[0].plot()  # Plot predictions on the image
+
+    # Show the annotated image using OpenCV
+    window_name = "YOLOv8 Prediction"
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, 1024, 1024)
+    cv2.imshow(window_name, annotated_img)
+    cv2.waitKey(0)  # Press any key to close
+    cv2.destroyAllWindows()
 
 
 def main():
     model = YOLO("runs/detect/train3/weights/best.pt").to('cuda')
 
-    # result_grid = model.train(
-    #     data='manga109.yaml',
-    #     epochs=100,
-    #     patience=5,
-    #     batch=8,
-    #     #nbs=64,
-    #     dropout=.05,
-    #     imgsz=1024,
-    #     augment=True,
-    #     val=True,
-    #     save=True,
-    #     plots=True,
-    #     verbose=True,
-    #     device='cuda'
-    # )
+    result_grid = model.train(
+        data='manga109.yaml',
+        epochs=30,
+        patience=5,
+        batch=12,
+        #nbs=64,
+        dropout=.05,
+        imgsz=1024,
+        augment=True,
+        val=True,
+        save=True,
+        plots=True,
+        verbose=True,
+        device='cuda'
+    )
 
+    # aggregate runs
+    aggregate_run_results()
 
-    img_path = f"Manga109_YOLO/test/images/AisazuNihaIrarenai_017.jpg"
-    # Predict and return the result
-    results = model.predict(source=img_path, device='cuda')
-
-    # Load and display the original image with predictions
-    img = cv2.imread(img_path)
-    annotated_img = results[0].plot()  # Plot predictions on the image
-
-    # Show the annotated image using OpenCV
-    cv2.imshow("YOLOv8 Prediction", annotated_img)
-    cv2.waitKey(0)  # Press any key to close
-    cv2.destroyAllWindows()
+    test(model,"Manga109_YOLO/test/images/AisazuNihaIrarenai_017.jpg")
+    test(model, "Manga109_YOLO/test/images/AisazuNihaIrarenai_018_left.jpg")
 
 
 
